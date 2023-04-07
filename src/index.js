6750b5a8b57816aa53939075a41935dc85157dfa
Chris <chrismuto@Chriss-Mac-mini.local>
2023-04-07 10:45:28 -0700

    package exporting function, only needed files publish now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import locationFinder from "./locationFinder.js";
-
-const testFunction = (city) => {
-   return locationFinder(city);
+async function locationFinder(newCity) {
+   var geolocateApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + newCity + "&limit=1&appid=d06d736f70b1c7547ee6d36a7c3c8929";
+   fetch(geolocateApi)
+   .then(function (response) {
+       return response.json();
+   })
+   .then(function (data) {
+       console.log(data);
+   });
 }
 
-module.exports = { testFunction };
+export { locationFinder }
